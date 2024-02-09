ad369b3251b7333840a7d6f8bd880f49ce863454
Benana <sharpeye5215@gmail.com>
2024-02-09 20:25:34 +0000

    Initial Commit
    Includes an empty Unreal Engine 5.3 c++ project as well as the Git source control plugin.

---
+++ Source/CatBoomerShooter/CatBoomerShooter.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CatBoomerShooter.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CatBoomerShooter, "CatBoomerShooter" );
